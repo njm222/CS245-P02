fa7032c6c743328a0de54af9ca813b9e546d6f4a
Nathan <nathan.john.menezes@gmail.com>
2017-09-02 14:00:04 -0700

    Added Binary Iterative Search

---
+++ BinaryIterativeSearch.java
@@ -0,0 +1,28 @@
+package edu.usfca.cs.cs245;
+
+public class BinaryIterativeSearch implements Practice2Search {
+
+	public String searchName() {
+		// TODO Auto-generated method stub
+		return "binary-iterative";
+	}
+
+	public int search(int[] arr, int target) {
+		// TODO Auto-generated method stub
+		int min = 0;
+		int max = arr.length - 1;
+		int mid;
+		
+		while(min <= max){
+			mid = (min + max) / 2;
+			if(arr[mid] == target)
+				return mid;
+			if(arr[mid] > target)
+				max = mid - 1;
+			else
+				min = mid + 1;
+		}
+		return -1;
+	}
+
+}
